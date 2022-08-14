8a193cb982b8dd2c62aba5bf48e10759518422cb
Reaper2403 <2005153@kiit.ac.in>
2022-08-14 17:25:21 +0530

    added server address

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ import socket
 import threading
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('192.168.29.112', 55555))
+client.connect(("172.104.207.82", 55555))
 nick = input('Enter your nickname please to enter the chat: ')
 
 
